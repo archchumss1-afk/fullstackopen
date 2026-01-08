d2f715fccbc4c84e78e286d17012e8945928e3ca
archchumss1-afk <archchu.mss1@gmail.com>
2026-01-08 12:59:43 +0530

    Add fullstackopen part 2.1-2.5 exercises

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,42 @@
+const Header=({name})=>{
+    return(
+        <div>
+            <h2>{name}</h2>
+        </div>
+    )
+}
+const Part=({part})=>{
+    return(
+     <div>
+       <p> {part.name} {part.exercises}</p>
+     </div>
+    )
+}
+const Content =({parts})=>{
+    return(
+        <div>
+            {parts.map((part)=><Part key={part.id} part={part}/>)}
+        </div>
+
+    )
+}
+const Total=({parts})=>{
+    const total=parts.reduce((sum,part)=>sum+part.exercises,0)
+     return(
+        <div>
+            <strong>total of {total} exercises</strong>
+        </div>
+     )
+}
+
+const Course =({course})=>{
+    return(
+        <div>
+            <Header name={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
+
+}
+export default Course
