8dd9f8329b66a2279c5605b4edc4cf94890ffe3f
archchu.mss1@gmail.com <archchu.mss1@gmail.com>
2026-06-27 00:15:07 +0530

    complete the 3.11 exercise

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fullstackopen
+"Deployment blocked due to Render card verification"  // unable to finish exercise 3.10
--- part3-phonebook-backend/index.js
+++ part3-phonebook-backend/index.js
@@ -7,6 +7,8 @@ const app =express()
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static('dist'))
+
 morgan.token('body',(request,response)=>{
     if(request.method === 'POST'){
         return JSON.stringify(request.body)
